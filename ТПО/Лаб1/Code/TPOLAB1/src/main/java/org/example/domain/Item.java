26bfbb89d8a50e4fa2f147d2ed15d7dff4ab016d
AlexandroLe <alex-yastrebov2003-2015@mail.ru>
2026-03-05 19:52:38 +0400

    fix: new 3 part

--- ТПО/Лаб1/Code/TPOLAB1/src/main/java/org/example/domain/Item.java
+++ ТПО/Лаб1/Code/TPOLAB1/src/main/java/org/example/domain/Item.java
@@ -1,17 +1,4 @@
 package org.example.domain;
 
-public class Item {
-    private final String name;
-    private final String material;
-
-    public Item(String name, String material) {
-        this.name = name;
-        this.material = material;
-    }
-
-    public String getName() { return name; }
-    public String getMaterial() { return material; }
-
-    @Override
-    public String toString() { return "Item{" + name + ":" + material + "}"; }
+public record Item(String name, String material) {
 }
